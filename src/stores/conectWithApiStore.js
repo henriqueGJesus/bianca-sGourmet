77abf551d689aeaf8e500eb82c1d914a0a1d6b0c
henriqueGJesus <hj30032006@gmail.com>
2024-09-17 21:28:55 -0300

    será que so funciona no vercel?

--- src/stores/conectWithApiStore.js
+++ src/stores/conectWithApiStore.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 export const useConectStore = defineStore('conectStore', {
   state: () => ({
     comments: [],
-    url: "http://localhost:8000"
+    url: "https://bianca-s-gourmet-py-6p2c.vercel.app"
   }),
 
   actions: {
